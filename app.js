de7ca9740fffac2631a78ed375b40a16591c575a
yegorf1 <yegorf1@gmail.com>
2017-01-06 22:40:50 +0300

    Simple app

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var app = require('express')();
-var http = require('http').Server(app);
+var http = require('http').createServer(app);
+var io = require('socket.io').listen(http);
+var serveStatic = require('serve-static');
+var constants = require('./constants');
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+function listening() {
+    console.log(`Server running on ${constants.HTTP_PORT}...`);
+}
+
+app.use(serveStatic(__dirname + '/static'));
+
+io.on('connection', function(socket) {
+    console.log('123');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+http.listen(constants.HTTP_PORT, listening);
